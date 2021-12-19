719d8a3231ba272fc818844a90efe51da317ed5f
Yasu Flores <carlosyasu91@gmail.com>
2021-12-18 18:21:19 -0600

    start creating tab selector and adding styles

---
+++ src/TabSelector.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import classnames from "classnames";
+
+import "./TabSelector.css";
+
+function TabSelector(props) {
+  const { tabs } = props;
+  const [tabSelected, setTabSelected] = useState(0);
+
+  const onTabClicked = (tab) => {
+    setTabSelected(tab);
+  };
+
+  return (
+    <div className="tab-selector">
+      <ul>
+      {
+        tabs.map((tab, index) => {
+          return (
+            <button onClick={() => onTabClicked(index)} className={classnames({ selected: index === tabSelected})}>
+              <li className="tab">Element {index}</li>
+            </button>
+          )
+        })
+      }
+      </ul>
+    </div>
+  );
+}
+
+export default TabSelector;
